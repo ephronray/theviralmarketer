d1e4084579b5c2ab6a085630baa8f96af07399c0
ephronray <saadraza351@gmail.com>
2020-08-26 02:33:03 -0700

    edit add tweet

--- viral-tweets/action/addTweetProgress.php
+++ viral-tweets/action/addTweetProgress.php
@@ -29,7 +29,7 @@ if(isset($_POST['submit']))
      $imageError = false;
      $imageMessage="";
      $caption = $_POST['caption'];
-     //if($_SESSION['user']['ibm'] != 'IBM1') {
+     if($_SESSION['user']['ibm'] != 'IBM1') {
 	  if(!empty($paid_facility_list)){
 				foreach($paid_facility_list as $paid_item){
 			 if((($paid_item['slug'] == MembershipConstant::WATERMARK_FOR_TWITTER ) && ($paid_item['is_show'] == 1)) || $paid_item['slug'] != MembershipConstant::WATERMARK_FOR_TWITTER ) {
@@ -40,7 +40,7 @@ if(isset($_POST['submit']))
 
              } 
             
-         //   }
+            }
 	
 	$datepicker = null; 
      $catagory_id = $_POST['catagory'];
