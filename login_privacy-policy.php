537f8a34c1b52e7366397a2e7e8760a861c7df7a
Lakshan Viranga <lakshanviranga0000@gmail.com>
2020-09-16 17:09:49 +0530

    fixed-issues

--- login_privacy-policy.php
+++ login_privacy-policy.php
@@ -10,52 +10,77 @@ $result    = mysqli_query($newsifyObj->dbCon, $sql);
 $row = mysqli_fetch_array($result);
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-  <head>
-    <title>The Viral Markter</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+  <title>The VIRALMARKETER</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/jquery-ui.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+  <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+  <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+  <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+  <link rel="stylesheet" href="css/aos.css">
+
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+
     
-    
-    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet">
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <header class="site-navbar js-sticky-header site-navbar-target" role="banner" style="background-color: #e6ccff;">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/jquery-ui.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-
-    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
-
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/video.css">
-
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-
-    <link rel="stylesheet" href="css/aos.css">
-
-    <link rel="stylesheet" href="css/style.css">
-    
-  </head>
-  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
-  
-  <div class="site-wrap"  id="home-section">
-    
-
-    <div class="site-blocks-cover" style="overflow: hidden;">
       <div class="container">
-          <div class="jumbotron jumbotron-fluid" style="margin-top: 10%">
+        <div class="row align-items-center">
+
+          <div class="col-6 col-xl-2">
+            <div class="mb-0 site-logo"><img src="assets/logo_black.png" alt=""></div>
+          </div>
+<!-- 
+          <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3 text-black"></span></a></div> -->
+
+        </div>
+      </div>
+
+    </header>
+
+
+
+    <div class="site-section hero" id="home-section">
+      <div class="container">
+         <div class="jumbotron jumbotron-fluid" style="margin-top: 10%">
             <div class="container">
-              <h1 class="display-4">Privacy Policy</h1>
+              <h1 class="display-4"><strong>PRIVACY AND POLICY</strong></h1>
               <div class="col-md-12">
                     <?php if(!empty($row['page_content'])){
 						echo $row['page_content'];
 					} else { ?>
 						<div class="alert alert-info fade in ">
 							<strong><i class="fa fa-info-circle" aria-hidden="true"></i></strong>&nbsp;&nbsp;
-							No, FAQ Found!!
+							No, Privacy policy Found!!
                         </div>
 					<?php }?>
                 </div>
@@ -64,52 +89,28 @@ $row = mysqli_fetch_array($result);
       </div>
     </div>
 
+    
+    
 
-    <div class="site-section" id="features-section">
 
-    </div>
 
-    <div class="footer py-5 text-center">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-12">
-            <p class="mb-0">
-              <a href="#" class="p-3"><span class="icon-facebook"></span></a>
-              <a href="#" class="p-3"><span class="icon-twitter"></span></a>
-              <a href="#" class="p-3"><span class="icon-instagram"></span></a>
-              <a href="#" class="p-3"><span class="icon-linkedin"></span></a>
-              <a href="#" class="p-3"><span class="icon-pinterest"></span></a>
-            </p>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12">
-            <p class="mb-0">
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved </a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> <!-- .site-wrap -->
+    </div> <!-- .site-wrap -->
 
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/jquery-ui.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.countdown.min.js"></script>
-  <script src="js/bootstrap-datepicker.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.fancybox.min.js"></script>
-  <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
 
-  <script src="js/video.js"></script>
-  
-  <script src="js/main.js"></script>
-  
+
+    <script src="js/main.js"></script>
+
+
   </body>
-</html>
+  </html>
