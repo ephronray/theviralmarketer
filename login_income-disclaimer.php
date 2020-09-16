6203806598eb2d64c24a3496c5bb62bc1dfd5e6b
ephronray <69852461+ephronray@users.noreply.github.com>
2020-09-16 20:27:50 +0500

    Comma Issue

--- login_income-disclaimer.php
+++ login_income-disclaimer.php
@@ -4,7 +4,7 @@ $newsifyObj = new  dbConnect();
 $sql = "SELECT 
 		  id, page_name, page_content 
 		  FROM  `tbl_pages` 
-		  WHERE  `page_name` =  'income-disclaimer Limit 1";
+		  WHERE  `page_name` =  'income-disclaimer' Limit 1";
 //$query   = $newsifyObj->db_select($sql);
 $result    = mysqli_query($newsifyObj->dbCon, $sql);
 $row = mysqli_fetch_array($result);
