8d814f1fa7234b21ee6a89c61557ee1c3e4de373
Saad Raza <saadraza.official@gmail.com>
2020-09-25 04:59:14 +0500

    edit buy bitcoin

--- _libs/TransferWise/includes/configure.php
+++ _libs/TransferWise/includes/configure.php
@@ -16,7 +16,7 @@ define('SANDBOX_TOKEN_READONLY','09b85b07-7392-4abe-b381-dadf94abc7a6');
 define('SANDBOX_TOKEN_FULL'    ,'09b85b07-7392-4abe-b381-dadf94abc7a6'); 
 
 //define('SANDBOX_ID_PERSONAL','');
-define('SANDBOX_ID_BUSINESS','21773320');
+define('SANDBOX_ID_PERSONAL','21773320');
 //define('PROFILE_ID_','1');
 
 
