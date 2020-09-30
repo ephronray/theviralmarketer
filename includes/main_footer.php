8b19ff07974b227015b1dd53d0ff7dd2b4d23952
Saad Raza <saadraza.official@gmail.com>
2020-10-01 00:04:03 +0500

    main_footerphp

--- includes/main_footer.php
+++ includes/main_footer.php
@@ -77,8 +77,6 @@
 	<script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
 	
-	<script src="<?=$newsifyObj->base_url; ?>/js/js/pages/dashboard-chart.js"></script>
-	
 	<!-- Crypto_Admin for demo purposes -->
 	<script src="<?=$newsifyObj->base_url; ?>/js/js/demo.js"></script>
 	<!--tags Input-->
