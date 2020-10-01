02fe7072056fcf8d478a46b1c1d2cdeccd9c1b18
Saad Raza <saadraza.official@gmail.com>
2020-10-01 18:39:26 +0500

    edit man_footer

--- includes/main_footer.php
+++ includes/main_footer.php
@@ -76,9 +76,13 @@
 	<script src="<?=$newsifyObj->base_url; ?>/js/js/template.js"></script>
 	
 	<!-- Crypto_Admin dashboard demo (This is only for demo purposes) -->
+	<script src="<?=$newsifyObj->base_url; ?>/js/js/pages/dashboard.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+	
+	<script src="<?=$newsifyObj->base_url; ?>/js/js/pages/dashboard-chart.js"></script>
 	
 	<!-- Crypto_Admin for demo purposes -->
-	<script src="<?=$newsifyObj->base_url; ?>/js/js/demo.js"></script>
 	<!--tags Input-->
 	<script type="text/javascript" src="<?= $base_url ?>/js/tagsinput.js"></script>  
 	<script src="<?=$newsifyObj->base_url; ?>/js/viral-tweet/viral_tweet_ajax.js"></script>
