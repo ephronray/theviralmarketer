f497d3d457d7291f42b26d122f739cb9095fdde5
Saad Raza <saadraza.official@gmail.com>
2020-10-01 18:47:53 +0500

    edit man_footer

--- includes/main_footer.php
+++ includes/main_footer.php
@@ -86,7 +86,8 @@
 	<script src="<?=$newsifyObj->base_url; ?>/js/js/demo.js"></script>
 	<!--tags Input-->
 	<script type="text/javascript" src="<?= $base_url ?>/js/tagsinput.js"></script>  
-	
+	<script src="<?=$newsifyObj->base_url; ?>/js/viral-tweet/viral_tweet_ajax.js"></script>
+
 	
 
 </body>
